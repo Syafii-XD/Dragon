c3b29b07dd247dd213af5e2fac02d4f15427c0c5
Syafii-XD <101574975+Syafii-XD@users.noreply.github.com>
2022-07-05 03:35:12 +0700

    Update run.py

--- run.py
+++ run.py
@@ -3,6 +3,6 @@ import os
 if __name__ == "__main__":
    try:
        os.system("git pull")
-       __import__("dmbf").main()
+       __import__("Crack").main()
    except Exception as e:
        exit(str(e))
